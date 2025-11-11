63fa6caed15e95c83ebdc2404c897a292b14fd2f
p7810456 <pranit.arya03@gmail.com>
2025-11-10 19:51:32 -0600

    Kalman filter converges with new h(x) and H matrix

--- src/Control.cpp
+++ src/Control.cpp
@@ -141,22 +141,24 @@ static float y_hyperbola(float n, float Q, float d_Q) {
 
 // n is in seconds, Q and dQ_cmd are arrays in Joules
 // Return value of expected temperature input at n seconds from now based on previous state and control history
-static float y_exp_use_hist(float n, float Q[MCP_N], int Q_index, float dQ_cmd[MCP_N], int dQ_index) {
+// buffer_element_num is how many elements are in the buffers given, since they won't necessarily be full e.g. on startup
+static float y_exp_use_hist(float n, float Q[MCP_N], int Q_index, float dQ_cmd[MCP_N], int dQ_index, int buffer_element_num) {
     float state_Q = 0.0f;
     
-    for(int i = 0; i < MCP_N; i++) {
+    for(int i = 1; i <= buffer_element_num; i++) {
         float hist_Q = Q[(Q_index - i) % MCP_N];
         float hist_dQ_cmd = dQ_cmd[(dQ_index - i) % MCP_N];
 
         float hist_T = hist_Q / MODEL_J_PER_C;
         float hist_dT = hist_dQ_cmd / MODEL_J_PER_C;
-
+        
         float state_diff = hist_T + (hist_dT - (exp(-0.125 * (i + (n * SAMPLES_PER_SEC) - 2)) * hist_dT));
+        printf("Hist T = %f, Hist dT = %f, hist_Q = %f, hist_dQ_cmd=%f, state_diff=%f\n", hist_T, hist_dT, hist_Q, hist_dQ_cmd, state_diff);
 
         state_Q += (state_diff >= 0) ? state_diff : 0;
     }
 
-    return state_Q;
+    return (state_Q / (float)buffer_element_num); // Average of all predicted values
 }
 
 void temp_control_task(void *pvParameters) {
@@ -227,14 +229,20 @@ void temp_control_task(void *pvParameters) {
     // Initial Prediction step
     Matrix X_p = A * X;
     Matrix P_p = ((A * P) * A.transpose()) + Q;
+    
+    Q_prev[(Q_prev_index++) % MCP_N] = X[0][0]; 
+    commanded_dQ_prev[(commanded_dQ_prev_index++) % MCP_N] = 0; // No control input given yet
 
     // Control input
     // extern uint8_t pump_state;
     // X_p[0][0] += (PID_TIME_MS / SSR_TIME_MS) * (ENERGY_PER_HALF_PHASE * u);
     // X_p[1][0] += (-625.304f * pump_state) + ((1000.0f / SSR_TIME_MS) * ENERGY_PER_HALF_PHASE * u); // Energy loss from pump putting water through TODO div 2 to account for timestep?    
-
+    
+    int iterations = 0; // How many iterations of the loop we have done, to account for some buffers not being full on startup
     // Measure thermistor and output on serial
     while(1) {
+        iterations++;
+
         therm_raw_avg = get_therm_adc_reading();
         curr_temp = adc_2_temp(therm_raw_avg);
 
@@ -252,7 +260,7 @@ void temp_control_task(void *pvParameters) {
         // Estimate step
         Matrix S(1, 1);
         // Matrix H(1, 2, (float[100]){y_logistic(PID_TIME_MS/1000.0f) / MODEL_J_PER_C, ((PID_TIME_MS/1000.0f) * y_logistic(PID_TIME_MS/1000.0f)) / MODEL_J_PER_C});
-        Matrix H(1, 2, (float[100]){1.0f / MODEL_J_PER_C, 1.0f / MODEL_J_PER_C});
+        Matrix H(1, 2, (float[100]){1.0f / MODEL_J_PER_C, (1.0f / MODEL_J_PER_C) - (0.004107)}); // dT/dQ = 1/J_per_C, dT/d(dQ) = (1 / J_PER_C) - exp(0.25) / J_PER_C
         S = ((H * P_p) * H.transpose()) + Matrix(1, 1, 0.02); // 0.279f
         Matrix K(2, 1);
         Matrix S_inv(1, 1);
@@ -262,7 +270,8 @@ void temp_control_task(void *pvParameters) {
         Matrix Z(1, 1, curr_temp);
         // Matrix hx(1, 1, ((X[0][0] / MODEL_J_PER_C) + ((X_p[1][0] * y_logistic(PID_TIME_MS/1000.0f))/MODEL_J_PER_C))); // z - h(x_p)
         // Matrix hx(1, 1, y_hyperbola(PID_TIME_MS / 1000.0f, X_p[0][0], X_p[1][0])); // z - h(x_p)
-        Matrix hx(1, 1, y_exp_use_hist(PID_TIME_MS / 1000.0f, Q_prev, Q_prev_index, commanded_dQ_prev, commanded_dQ_prev_index)); // z - h(x_p)
+        int buf_elements = (iterations >= 50) ? 50 : iterations;
+        Matrix hx(1, 1, y_exp_use_hist(PID_TIME_MS / 1000.0f, Q_prev, Q_prev_index, commanded_dQ_prev, commanded_dQ_prev_index, buf_elements)); // z - h(x_p)
         X = X_p + (K * (Z - hx));
         P = P_p - ((K * H) * P_p);
 
@@ -323,14 +332,14 @@ void temp_control_task(void *pvParameters) {
         // Only output first predicted control output
         u = U[0];
 
-        // Store control input
-        commanded_dQ_prev[(commanded_dQ_prev_index++) % MCP_N] = (1.0f / u) * MODEL_J_PER_C;
-        
         u = (u < 0) ? 0 : (u > 60) ? 60 : u; // Bounds checking
 
         // Update energy put in
         curr_energy += (u * ENERGY_PER_HALF_PHASE); // Each half phase of control adds Joules to the system.
 
+        // Store control input
+        commanded_dQ_prev[(commanded_dQ_prev_index++) % MCP_N] = (u * ENERGY_PER_HALF_PHASE);
+        
         // Update predicted y(t) and dy/dt based on the calculated control input
         // Zero out last index in ring buffer since that's the new "frontier" of our current ring buffer
         // Y_pred[(dY_pred_index - 1) % MCP_N] = 0.0f;
@@ -362,12 +371,14 @@ void temp_control_task(void *pvParameters) {
         // Prediction step
         // X_p = A * X;
         extern uint8_t pump_state;
-        X_p = A * ((Matrix(2, 1, (float[100]){X[0][0], (-625.304f * pump_state) + ((1000.0f / SSR_TIME_MS) * ENERGY_PER_HALF_PHASE * u)})));
+        // X_p = A * ((Matrix(2, 1, (float[100]){X[0][0], (-625.304f * pump_state) + ((1000.0f / SSR_TIME_MS) * ENERGY_PER_HALF_PHASE * u)})));
+        X_p = A * ((Matrix(2, 1, (float[100]){X[0][0], X[1][0]})));
+
+        // Control input
+        X_p[0][0] += (-625.304f * pump_state) + ((PID_TIME_MS / SSR_TIME_MS) * (ENERGY_PER_HALF_PHASE * u));
+        // X_p[1][0] += (-625.304f * pump_state) + ((1000.0f / SSR_TIME_MS) * ENERGY_PER_HALF_PHASE * u); // Energy loss from pump putting water through TODO div 2 to account for timestep?
 
         X_p.print();
-        // Control input
-        // X_p[0][0] += (PID_TIME_MS / SSR_TIME_MS) * (ENERGY_PER_HALF_PHASE * u);
-        // X_p[1][0] += (-625.304f * pump_state) + ((1000.0f / SSR_TIME_MS) * ENERGY_PER_HALF_PHASE * u); // Energy loss from pump putting water through TODO div 2 to account for timestep?
 
         // Matrix P_p(2, 2);
         P_p = ((A * P) * A.transpose()) + Q;
